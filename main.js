aedbd1a4b5329fb90c0b6e685127a0da6f1eab44
NdunguSam01 <ndungu.muigai01@gmail.com>
2022-10-06 17:34:54 +0300

    First commit. Yet to clear form after input

---
+++ main.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded',function()
+{
+    const list=document.querySelector('#movie-list ul');
+    const form=document.forms;
+
+    //Deleting a movie
+    list.addEventListener('click',(e)=>
+    {
+        if(e.target.className=='delete')
+        {
+            const li=e.target.parentElement;
+            li.parentNode.removeChild(li);
+            alert('Movie deleted');
+        }
+    });
+
+    //Adding a movie
+    const addForm=form['new-movie'];
+    addForm.addEventListener('submit', function(e)
+    {
+        e.preventDefault();//cancels event if the event is ccancelled
+        
+        //Creating a new li element
+        const value = addForm.querySelector('input[type="text"]').value;
+        const li=document.createElement('li');
+        const movieName=document.createElement('span');
+        const deleteBtn=document.createElement('span');
+
+        //Setting text content
+        movieName.textContent = value;
+        deleteBtn.textContent = "delete";
+
+        //Creating the new classes
+        movieName.classList.add('name');
+        deleteBtn.classList.add('delete');
+
+        //Appending to DOM
+        li.appendChild(movieName);
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+    });
+})
